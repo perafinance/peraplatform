3ea44e95be7a685188df1fa1c833128d964cd059
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-01-22 02:35:37 +0300

    commenting and style guidince

--- contracts/TFFactory.sol
+++ contracts/TFFactory.sol
@@ -4,9 +4,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./TradeFarming.sol";
 
+/// @author Ulaş Erdoğan
+/// @title Trade Farming Factory Contract 
+/// @dev Deploys new trade farming contracts and store their addresses by transferring its ownership to msg.sender
 contract TradeFarmingFactory is Ownable {
+    // The addresses of deployed trade farming contracts
     address[] public createdContracts;
 
+
+    /**
+     * @notice Factory function - takes the parameters of the competition of Token - Avax pairs
+     * @param _routerAddress address of the DEX router contract
+     * @param _tokenAddress IERC20 - address of the token of the pair
+     * @param _rewardAddress IERC20 - address of the reward token
+     * @param _previousVolume uint256 - average of previous days
+     * @param _previousDay uint256 - previous considered days
+     * @param _totalDays uint256 - total days of the competition
+     * @param _owner address - the address which will be the owner
+     */
     function createTnAPair(
         address _routerAddress,
         address _tokenAddress,
@@ -17,6 +32,7 @@ contract TradeFarmingFactory is Ownable {
         address _owner
     ) external onlyOwner {
         TradeFarming TFcontract;
+        // Deploying the contract
         TFcontract = new TradeFarming(
             _routerAddress,
             _tokenAddress,
@@ -25,10 +41,16 @@ contract TradeFarmingFactory is Ownable {
             _previousDay,
             _totalDays
         );
-        TFcontract.transferOwnership(msg.sender);
+        // Transferring the ownership of the contract to the msg.sender
+        TFcontract.transferOwnership(_owner);
+        // Storing the address of the contract
         createdContracts.push(address(TFcontract));
     }
 
+    /**
+    * @notice Easily access the last created contracts address
+    * @return address - the address of the last deployed contract
+    */
     function getLastContract() external view returns (address) {
         return createdContracts[createdContracts.length - 1];
     }
